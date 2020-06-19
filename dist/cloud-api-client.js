a616cad2740ec43ffd8c883d6df05a7fd22933fb
Marek Sierociński <marek.sierocinski@bitbar.com>
2020-06-19 14:52:40 +0200

    Added formData method to APIEntity

--- dist/cloud-api-client.js
+++ dist/cloud-api-client.js
@@ -1,4 +1,4 @@
-/* Bitbar Cloud API Client for JavaScript v0.19.0 | (c) Bitbar Technologies and contributors | https://github.com/bitbar/cloud-api-client-js/blob/master/LICENSE.md */
+/* Bitbar Cloud API Client for JavaScript v0.20.0 | (c) Bitbar Technologies and contributors | https://github.com/bitbar/cloud-api-client-js/blob/master/LICENSE.md */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('@bitbar/finka'), require('axios'), require('qs')) :
   typeof define === 'function' && define.amd ? define(['@bitbar/finka', 'axios', 'qs'], factory) :
@@ -11,7 +11,7 @@
 
   finka();
 
-  var version = "0.19.0";
+  var version = "0.20.0";
 
   /*! *****************************************************************************
   Copyright (c) Microsoft Corporation. All rights reserved.
@@ -143,6 +143,12 @@
       APIEntity.prototype.jsonData = function (data) {
           this.headers({
               'Content-Type': 'application/json'
+          }).data(data);
+          return this;
+      };
+      APIEntity.prototype.formData = function (data) {
+          this.headers({
+              'Content-Type': 'multipart/form-data'
           }).data(data);
           return this;
       };
