c5c44bcb27d38eb5a5ab90672f077756f61dedb3
Marek Sierociński <marek.sierocinski@bitbar.com>
2021-01-29 02:53:17 +0100

    fix: SDCC-1129 - Admin/User/Services - cannot deactivate user service

--- dist/bitbar-cloud-api-client.js
+++ dist/bitbar-cloud-api-client.js
@@ -1,4 +1,4 @@
-/* @bitbar/cloud-api-client v0.28.0 | Copyright 2021 (c) SmartBear Software and contributors | .git/blob/master/LICENSE */
+/* @bitbar/cloud-api-client v0.28.1 | Copyright 2021 (c) SmartBear Software and contributors | .git/blob/master/LICENSE */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('@bitbar/finka'), require('axios'), require('qs')) :
   typeof define === 'function' && define.amd ? define(['@bitbar/finka', 'axios', 'qs'], factory) :
@@ -11,7 +11,7 @@
 
   finka();
 
-  var version = "0.28.0";
+  var version = "0.28.1";
 
   var ALLOWED_HTTP_METHODS;
   (function (ALLOWED_HTTP_METHODS) {
@@ -1199,7 +1199,7 @@
               throw new Error('Resource ID cannot be null!');
           }
           super(parent);
-          this.push('account-services', id);
+          this.push('admin', 'account-services', id);
       }
       activate() {
           return new APIResource(this).push('activate').post();
