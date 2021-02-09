0ff6a65b9691621fd78dd6f6647b1ea761f35c4a
Marek Sierociński <marek.sierocinski@bitbar.com>
2021-02-09 17:10:01 +0100

    Version 0.29.0

---
+++ dist/api/APIAdminListRuns.d.ts
@@ -0,0 +1,7 @@
+import APIList from './APIList';
+import APIResource from './APIResource';
+declare class APIAdminListRuns extends APIList {
+    constructor(parent: object);
+    config(): APIResource;
+}
+export default APIAdminListRuns;
