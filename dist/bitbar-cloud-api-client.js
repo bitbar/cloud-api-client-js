30aebfaa169e6c9d9553a9919135782d3f5cab63
Damian Sniezek <damian.sniezek@bitbar.com>
2021-04-09 09:30:30 +0200

    Version 0.36.0

--- dist/bitbar-cloud-api-client.js
+++ dist/bitbar-cloud-api-client.js
@@ -1,4 +1,4 @@
-/* @bitbar/cloud-api-client v0.35.0 | Copyright 2021 (c) SmartBear Software and contributors | .git/blob/master/LICENSE */
+/* @bitbar/cloud-api-client v0.36.0 | Copyright 2021 (c) SmartBear Software and contributors | .git/blob/master/LICENSE */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('@bitbar/finka'), require('axios'), require('qs')) :
   typeof define === 'function' && define.amd ? define(['@bitbar/finka', 'axios', 'qs'], factory) :
@@ -11,7 +11,7 @@
 
   finka();
 
-  var version = "0.35.0";
+  var version = "0.36.0";
 
   var ALLOWED_HTTP_METHODS;
   (function (ALLOWED_HTTP_METHODS) {
@@ -1048,15 +1048,6 @@
       }
       uiPreferences() {
           return new APIResource(this).push('ui-preferences');
-      }
-      integrations() {
-          return new APIList(this).push('integrations');
-      }
-      integration(id) {
-          if (id == null) {
-              throw new Error('Resource ID cannot be null!');
-          }
-          return new APIResource(this).push('integrations', id);
       }
       deviceUsage() {
           return new APIList(this).push('device-usage');
