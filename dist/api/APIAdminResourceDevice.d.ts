c9c8c839422131d7c23cb4843835cc2399c7f065
monikacil <monika.cilinska@bitbar.com>
2021-07-22 12:10:21 +0200

    version 0.38.4

--- dist/api/APIAdminResourceDevice.d.ts
+++ dist/api/APIAdminResourceDevice.d.ts
@@ -3,5 +3,8 @@ import APIList from './APIList';
 declare class APIAdminResourceDevice extends APIResource {
     constructor(parent: object, id: number);
     queue(): APIList;
+    cleanupConfiguration(): APIResource;
+    labels(): APIList;
+    label(id: number): APIResource;
 }
 export default APIAdminResourceDevice;
