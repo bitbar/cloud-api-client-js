b2608fe19574536c9f2f3ec2fafa31fca1db9a95
monikacil <monika.cilinska@bitbar.com>
2021-07-22 13:31:45 +0200

    version 0.39,1

--- dist/bitbar-cloud-api-client.js
+++ dist/bitbar-cloud-api-client.js
@@ -1,4 +1,4 @@
-/* @bitbar/cloud-api-client v0.38.4 | Copyright 2021 (c) SmartBear Software and contributors | .git/blob/master/LICENSE */
+/* @bitbar/cloud-api-client v0.39.1 | Copyright 2021 (c) SmartBear Software and contributors | .git/blob/master/LICENSE */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('@bitbar/finka'), require('axios'), require('qs')) :
   typeof define === 'function' && define.amd ? define(['@bitbar/finka', 'axios', 'qs'], factory) :
@@ -11,7 +11,7 @@
 
   finka();
 
-  var version = "0.38.4";
+  var version = "0.39.1";
 
   var ALLOWED_HTTP_METHODS;
   (function (ALLOWED_HTTP_METHODS) {
