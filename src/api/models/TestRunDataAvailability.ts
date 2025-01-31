dc379028ea2db04fbf639d3a34a1469643c4ccc6
Piotr Sapiejewski <piotr.sapiejewski@smartbear.com>
2025-01-31 15:11:33 +0100

    SDCB-13662 fix availabilities type

--- src/api/models/TestRunDataAvailability.ts
+++ src/api/models/TestRunDataAvailability.ts
@@ -2,7 +2,7 @@ import {DeviceSessionDataAvailability} from './DeviceSessionDataAvailability';
 import {QueryParams} from './HTTP';
 
 export type TestRunDataAvailability = {
-  deviceRunDataAvailabilities: DeviceSessionDataAvailability;
+  deviceRunDataAvailabilities: DeviceSessionDataAvailability[];
   id: number;
   testRunId: number;
 }
