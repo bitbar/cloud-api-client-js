7bed684a72066f36c4a46b152c846ff1e9da0165
Marietta Suchanek <marietta.suchanek@smartbear.com>
2025-03-26 13:39:05 +0100

    SDCB-14886: Extend DeviceCleanupConfiguration model

--- dist/api/models/CleanupConfiguration.d.ts
+++ dist/api/models/CleanupConfiguration.d.ts
@@ -1,5 +1,5 @@
 import { DeviceCleanupConfiguration } from './Device';
-export type CleanupConfigurationData = Pick<DeviceCleanupConfiguration, 'content' | 'discriminator' | 'enabled' | 'osType'>;
+export type CleanupConfigurationData = Pick<DeviceCleanupConfiguration, 'content' | 'discriminator' | 'enabled' | 'osType' | 'description'>;
 export interface SpecificCleanupConfigurationQueryParams {
     serialId: string;
 }
