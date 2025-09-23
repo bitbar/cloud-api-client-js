6e6f6dc08cdea9ef25176418f0946bcd5250e590
Piotr Sapiejewski <piotr.sapiejewski@smartbear.com>
2025-09-23 14:28:42 +0200

    SDCB-14049 Admin Account POST data type fix

--- dist/api/APIAdminResourceAccount.d.ts
+++ dist/api/APIAdminResourceAccount.d.ts
@@ -1,12 +1,12 @@
 import { APIAdminResource } from './APIAdminResource';
 import { APIList } from './APIList';
 import { APIResource } from './APIResource';
-import { Account, AccountUsage, AccountUsageParams, AccountUsageSummary, AccountUsageSummaryParams } from './models/Account';
-import { NoData, NoQueryParams, SimpleCollectionResponse } from './models/HTTP';
+import { Account, AccountData, AccountUsage, AccountUsageParams, AccountUsageSummary, AccountUsageSummaryParams } from './models/Account';
+import { NoQueryParams, SimpleCollectionResponse } from './models/HTTP';
 import { Role, RoleData, RoleParams } from './models/Role';
 import { AccountService } from './models/AccountService';
 import { AccountPreferences } from './models/AccountPreference';
-export declare class APIAdminResourceAccount extends APIResource<Account, NoQueryParams, NoData> {
+export declare class APIAdminResourceAccount extends APIResource<Account, NoQueryParams, AccountData> {
     constructor(parent: APIAdminResource, id: number);
     roles(): APIList<Role, RoleParams, RoleData>;
     role(id: number): APIResource<Role, RoleParams, void>;
